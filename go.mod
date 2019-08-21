392061c77191c82c89111ccdcfb70df50538b1da
Hugo Arregui <hugo@decentraland.org>
2019-08-21 09:14:55 -0300

    Add ICE transport and SCTP transport stats
    bytes received, bytes sent

--- go.mod
+++ go.mod
@@ -5,12 +5,12 @@ go 1.12
 require (
 	github.com/pion/datachannel v1.4.5
 	github.com/pion/dtls v1.5.1
-	github.com/pion/ice v0.5.10
+	github.com/pion/ice v0.5.11-0.20190821155750-ca101211acdc
 	github.com/pion/logging v0.2.2
 	github.com/pion/quic v0.1.1
 	github.com/pion/rtcp v1.2.1
 	github.com/pion/rtp v1.1.3
-	github.com/pion/sctp v1.6.7
+	github.com/pion/sctp v1.6.8-0.20190821155853-38bf6d805d7b
 	github.com/pion/sdp/v2 v2.3.0
 	github.com/pion/srtp v1.2.6
 	github.com/pion/transport v0.8.6
--- sctptransport.go
+++ sctptransport.go
@@ -4,6 +4,7 @@ package webrtc
 
 import (
 	"errors"
+	"time"
 	"io"
 	"math"
 	"sync"
@@ -284,4 +285,25 @@ func (r *SCTPTransport) State() SCTPTransportState {
 	r.lock.RLock()
 	defer r.lock.RLock()
 	return r.state
+}
+
+func (r *SCTPTransport) collectStats(collector *statsReportCollector) {
+	r.lock.Lock()
+	association := r.association
+	r.lock.Unlock()
+
+	collector.Collecting()
+
+	stats := TransportStats{
+		Timestamp: statsTimestampFrom(time.Now()),
+		Type:      StatsTypeTransport,
+		ID:        "sctpTransport",
+	}
+
+	if association != nil {
+		stats.BytesSent = association.BytesSent()
+		stats.BytesReceived = association.BytesReceived()
+	}
+
+	collector.Collect(stats.ID, stats)
 }
